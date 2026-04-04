0907a14dde8ef94e009f39b52ad6fc67e76a8e88
Vicky Goplani <goplanivicky34@gmail.com>
2026-04-05 03:58:53 +0530

    Backend Controllers- Final

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,5 +1,7 @@
 const productModel = require('../models/product.model');
 
+const normalizeCuisineName = (value = '') => value.trim();
+
 const listProducts = async (req, res, next) => {
   try {
     const data = await productModel.listProducts({
@@ -7,6 +9,24 @@ const listProducts = async (req, res, next) => {
     });
 
     res.status(200).json({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const listCuisines = async (req, res, next) => {
+  try {
+    const data = await productModel.listCuisines();
+    res.status(200).json({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createCuisine = async (req, res, next) => {
+  try {
+    const data = await productModel.createCuisine(normalizeCuisineName(req.body.name));
+    res.status(201).json({ success: true, data });
   } catch (error) {
     next(error);
   }
@@ -30,6 +50,14 @@ const getProductById = async (req, res, next) => {
 
 const createProduct = async (req, res, next) => {
   try {
+    const cuisine = await productModel.findCuisineByName(req.body.category);
+
+    if (!cuisine) {
+      const error = new Error('Select an existing cuisine or create a new one first.');
+      error.statusCode = 400;
+      throw error;
+    }
+
     const data = await productModel.createProduct(req.body);
     res.status(201).json({ success: true, data });
   } catch (error) {
@@ -39,6 +67,16 @@ const createProduct = async (req, res, next) => {
 
 const updateProduct = async (req, res, next) => {
   try {
+    if (req.body.category) {
+      const cuisine = await productModel.findCuisineByName(req.body.category);
+
+      if (!cuisine) {
+        const error = new Error('Select an existing cuisine or create a new one first.');
+        error.statusCode = 400;
+        throw error;
+      }
+    }
+
     const data = await productModel.updateProduct(req.params.id, req.body);
     res.status(200).json({ success: true, data });
   } catch (error) {
@@ -57,6 +95,8 @@ const deleteProduct = async (req, res, next) => {
 
 module.exports = {
   listProducts,
+  listCuisines,
+  createCuisine,
   getProductById,
   createProduct,
   updateProduct,
